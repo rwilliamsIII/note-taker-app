4fe4986c6e27dc295f386667a5ba59ddc4e3c2d3
Robert Williams <robert@a3marketing.com>
2020-04-17 00:05:34 -0500

    adding pathing to pull up the two different html pages

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.use(express.json());
 
 
 
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
 
 
 
