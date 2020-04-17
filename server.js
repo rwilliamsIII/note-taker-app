fe61852775c72999525df5be6896e8ded3fa673d
Robert Williams <robert@a3marketing.com>
2020-04-16 22:34:10 -0500

    created server.js file and added barebones server code

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+console.log("Server listening on PORT " + PORT);
+});
