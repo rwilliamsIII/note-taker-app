26ea41c7f29315dc8f4e13568691c86c43eb4162
Robert Williams <robert@a3marketing.com>
2020-04-22 18:29:55 -0500

    created route to pull up index.html page

--- server.js
+++ server.js
@@ -53,13 +53,9 @@ app.delete("/api/notes/:id", function (req, res){
     });
 });
 
-
-
-
-
-
-
-
+app.get("*", function (req, res){
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(PORT, function() {
 console.log("Server listening on PORT " + PORT);
