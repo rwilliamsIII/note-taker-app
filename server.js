a1535f4d0edd367eceb62316fd4f50f6423958ff
Robert Williams <robert@a3marketing.com>
2020-04-22 18:34:31 -0500

    fixed typo on server.js page

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(espress.static("public"));
+app.use(express.static("public"));
 
 app.get("/notes", function(req, res){
     res.sendFile(path.join(__dirname, "public", "notes.html"));
